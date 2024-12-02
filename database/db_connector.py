eea290ebe80282ce275dddee442aa3a4cfd291b9
AmyRoys <114415869+AmyRoys@users.noreply.github.com>
2024-12-02 21:08:00 +0000

    update main to align with flask structure

--- database/db_connector.py
+++ database/db_connector.py
@@ -1,12 +1,10 @@
 import psycopg2 
 
 def get_db_connection():
-    conn = psycopg2.connect(
+    return psycopg2.connect(
         dbname="postgres",
         user="test_user",
         password="amydb",  
         host="localhost",
         port="5432"
     )
-    return conn
-
--- main.py
+++ main.py
@@ -4,14 +4,21 @@ from web_application import create_app
 
 app = create_app()
 
-if __name__ == "__main__":
+def run_collector():
+    """
+    Collect metrics and insert them into the database.
+    """
     device_id = "Device_1"
     metrics = collect_metrics(device_id)
     print(f"Collected Metrics: {metrics}")
     insert_metrics_to_db(metrics)
     print("Metrics inserted into the database!")
-    
-    with app.app_context():
-        app.run(host='0.0.0.0', port=5000)
+
+if __name__ == "__main__":
+    # Run metrics collection logic
+    run_collector()
+
+    # Start the Flask web application
+    app.run(host="0.0.0.0", port=5000)
 
 
