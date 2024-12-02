df6c996f8e11d0ee2f523e604bfd4dee64aedab9
AmyRoys <114415869+AmyRoys@users.noreply.github.com>
2024-12-02 21:20:16 +0000

    update main

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from collector_agent.pc_collector import collect_metrics
 from collector_agent.uploader_queue import insert_metrics_to_db
 from web_application import create_app
 
-app = create_app()
-
 def run_collector():
     """
     Collect metrics and insert them into the database.
@@ -15,10 +13,7 @@ def run_collector():
     print("Metrics inserted into the database!")
 
 if __name__ == "__main__":
-    # Run metrics collection logic
-    run_collector()
-
-    # Start the Flask web application
-    app.run(host="0.0.0.0", port=5000)
+   app = create_app()
+   app.run(host="0.0.0.0", port=5000)
 
 
